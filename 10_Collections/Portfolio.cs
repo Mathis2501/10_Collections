3db7d5ca9d4b91c1d38853fe62244c597ec1c228
MathiasBellerbySylve <MathiasBellerbySylve@DESKTOP-6M2QC1B>
2016-09-27 08:35:49 +0200

    Add project files.

---
+++ 10_Collections/Portfolio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Collections
+{
+    class Portfolio
+    {
+        public List<Asset> Stocks = new List<Asset>();
+        public Portfolio(List<Asset> stocks)
+        {
+            Stocks = stocks;
+        }
+
+        public Portfolio()
+        {
+
+        }
+
+        public double GetTotalValue()
+        {
+            double Total = 0;
+            foreach (Asset item in Stocks)
+            {
+                Total += item.GetValue();
+            }
+            return Total;
+        }
+
+        public void AddAsset(Asset OrigAsset)
+        {
+            Stocks.Add(OrigAsset);
+        }
+
+    }
+}
